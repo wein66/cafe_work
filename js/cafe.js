2d05ff14afdf1697c543e396eab3016b768412b9
wein66 <40013461+wein66@users.noreply.github.com>
2023-02-28 15:59:29 +0900

    list완료

--- js/cafe.js
+++ js/cafe.js
@@ -34,4 +34,80 @@ $(function(){
       $(this).addClass('active');
   });
 
+  //slide show
+  let slide = setInterval(mySlide, 10000);
+  $('.next').click(function(){
+     clearInterval(slide);
+     mySlide();
+     slide = setInterval(mySlide, 10000);
+  });
+
+  $('.prev').click(function(){
+      clearInterval(slide);
+      prevEvent();
+      slide = setInterval(mySlide, 10000);
+  });
+  myTime();
+
+//데이터 가져오기
+jQuery.ajax({
+   type: "GET", 
+   url: "./data/data.json",
+   dataType: "JSON", 
+   success: function(data){
+      let list = '';
+      for(let i = 0; i < data.cafelist.length; i++){
+         list += '<li><a href="#" class="d-flex align-items-center justify-content-between">';
+         list += '<div class="tbox d-flex align-items-center">';
+         list += '<img src="'+data.cafelist[i].img+'" alt="'+data.cafelist[i].num+'">';
+         list += '<h1>'+data.cafelist[i].num+'</h1><p class="ellipise">'+data.cafelist[i].content+'</p></div>';
+         list += '<div class="cfe d-flex"><p class="ellipise">'+data.cafelist[i].cafename+'</p>';
+         list += '<p class="dg">'+data.cafelist[i].comment+'</p></div>';
+         list += '</a></li>';
+      }
+      $('.clist').html(list);
+   },
+   error:function(xhr, status, error){
+      console.log(error);
+   }
+})
+
 });  //jquery
+
+function mySlide(){
+   const eq0 = $('.hero .new:eq(0)');  // 최초로 보이는 new
+   const eq1 = $('.hero .new:eq(1)');  // 그 뒤에 숨어 있는 new
+   //animate를 통해 두 번째 new에 zindex를 추가하여 제일 앞에 보이게 하고 
+   //투명처리 할 후 점차 진하게 보이게 한다.
+   eq1.addClass('zindex').css('opacity', 0).animate({  
+      'opacity': 1
+   }, 10, function(){
+      //animate작업이 끝나면 이전에 보였던 new의 zindex를 지우고 가장 나중으로 
+      //바꿔준다.
+      eq1.find('li').eq(ranDomList()).addClass('active');
+      eq0.removeClass('zindex');   
+      eq0.find('li').removeClass('active');
+      $('.hero').append(eq0);    
+   });
+}
+
+function ranDomList(){
+   return Math.floor(Math.random() * 4);
+}
+
+function prevEvent(){
+   $('.new:first-child').removeClass("zindex");
+   $('.new:last-child').addClass('zindex').clone().prependTo('.hero');
+   $('.new:last-child').remove();
+}
+
+function myTime(){
+   let dt = new Date();
+   let y = dt.getFullYear();
+   let m = dt.getMonth()+1;
+   let d = dt.getDate();
+   let h = dt.getHours();
+   let mm = dt.getMinutes();
+   let mt = `${y}.${m}.${d}.<strong>${h}:${mm}</strong>`;
+   $('.thetime').html(mt);
+}
